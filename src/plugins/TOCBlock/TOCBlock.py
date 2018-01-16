976becd3520c6ec98950403abcd64b22df728752
Matthias Müller <matthias.mueller@MacBook-Pro.local>
2018-01-16 18:58:00 +0100

    Move TOC styles in plugin initialisation

--- src/plugins/TOCBlock/TOCBlock.py
+++ src/plugins/TOCBlock/TOCBlock.py
@@ -5,8 +5,10 @@ sys.path.insert(0, '...')
 
 
 from reportlab.platypus import PageBreak
+from reportlab.lib.styles import ParagraphStyle
 from reportlab.platypus.tableofcontents import TableOfContents
 from reportlab.lib.units import cm
+from reportlab.lib.enums import TA_JUSTIFY, TA_LEFT, TA_RIGHT, TA_CENTER
 
 from common.PluginInterface import PluginInterface
 
@@ -19,7 +21,33 @@ class TOCBlock(PluginInterface):
         return 'toc'
 
     def init(self, context):
-        pass
+        # define used styles if not exists already
+        if not 'Toc0' in context.styleSheet:
+            context.styleSheet['Toc0'] = ParagraphStyle(name="Toc0",
+                                                        parent=context.styleSheet['default'],
+                                                        alignment=TA_LEFT,
+                                                        fontSize=10)
+
+        if not 'Toc1' in context.styleSheet:
+            context.styleSheet['Toc1'] = ParagraphStyle(name="Toc1",
+                                                        parent=context.styleSheet['default'],
+                                                        alignment=TA_LEFT,
+                                                        fontSize=10,
+                                                        leftIndent=0.5 * cm)
+
+        if not 'Toc2' in context.styleSheet:
+            context.styleSheet['Toc2'] = ParagraphStyle(name="Toc2",
+                                                        parent=context.styleSheet['default'],
+                                                        alignment=TA_LEFT,
+                                                        fontSize=10,
+                                                        leftIndent=1 * cm)
+
+        if not 'Toc3' in context.styleSheet:
+            context.styleSheet['Toc3'] = ParagraphStyle(name="Toc3",
+                                                        parent=context.styleSheet['default'],
+                                                        alignment=TA_LEFT,
+                                                        fontSize=10,
+                                                        leftIndent=1.5 * cm)
 
     def prepare(self, block, context):
         pass
