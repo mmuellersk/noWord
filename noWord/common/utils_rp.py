543c889d777ab19d3ee080e6d13045abfda6329d
Matthias Müller <matthias.mueller@MacBook-Pro.local>
2018-09-06 20:55:45 +0200

    Fix PEP8

--- noWord/common/utils_rp.py
+++ noWord/common/utils_rp.py
@@ -135,18 +135,19 @@ def getImage(filename, width, dummy=False):
 
     return DummyFlowable(Spacer(width, height), img, dummy)
 
+
 def optimalWidth(par, maxWidth, maxHeight):
-  w, minHeight = par.wrap(maxWidth, maxHeight)
-  totalMM = int(w / mm)
+    w, minHeight = par.wrap(maxWidth, maxHeight)
+    totalMM = int(w / mm)
 
-  while totalMM > 0:
-    totalMM -= 1
-    w, h = par.wrap(totalMM * mm, maxHeight)
-    if h > minHeight:
-      totalMM += 1
-      break
+    while totalMM > 0:
+        totalMM -= 1
+        w, h = par.wrap(totalMM * mm, maxHeight)
+        if h > minHeight:
+            totalMM += 1
+            break
 
-  return totalMM * mm
+    return totalMM * mm
 
 # This class provides a proxy that forwards all reportlab calls to a temporary or a final
 # embedded flowable, depending on its state. This allows to insert a temporary object and
@@ -406,26 +407,26 @@ class PageCountBlocker(Flowable):
 
 
 class VerticalText(Flowable):
-  def __init__(self, paragraph, xoffset=0, yoffset=0):
-    Flowable.__init__(self)
-    self.paragraph = paragraph
-    self.w = 0
-    self.xoffset = xoffset
-    self.yoffset = yoffset
+    def __init__(self, paragraph, xoffset=0, yoffset=0):
+        Flowable.__init__(self)
+        self.paragraph = paragraph
+        self.w = 0
+        self.xoffset = xoffset
+        self.yoffset = yoffset
 
-  def wrap(self, w, h):
-    w, h = self.paragraph.wrap(h, w)
-    self.w = h
-    self.h = optimalWidth(self.paragraph, 20*cm, 20*cm)
-    return self.w, self.h
+    def wrap(self, w, h):
+        w, h = self.paragraph.wrap(h, w)
+        self.w = h
+        self.h = optimalWidth(self.paragraph, 20*cm, 20*cm)
+        return self.w, self.h
 
-  def draw(self):
-    self.paragraph.wrap(self.h, self.w)
-    self.canv.saveState()
-    self.canv.translate(self.xoffset + self.w, self.yoffset)
-    self.canv.rotate(90)
-    self.paragraph.drawOn(self.canv, 0, 0)
-    self.canv.restoreState()
+    def draw(self):
+        self.paragraph.wrap(self.h, self.w)
+        self.canv.saveState()
+        self.canv.translate(self.xoffset + self.w, self.yoffset)
+        self.canv.rotate(90)
+        self.paragraph.drawOn(self.canv, 0, 0)
+        self.canv.restoreState()
 
 # Draw a text inside a colored circle, the hoffset can be used to fix the vertical
 # alignment of the text inside the circle, as paragraph object seems so report wrong
@@ -450,8 +451,10 @@ class Sticker(Flowable):
     def draw(self):
         self.canv.saveState()
         self.canv.setFillColor(self.backColor)
-        self.canv.circle(self.radius, self.radius, self.radius, stroke=0, fill=1)
-        self.paragraph.drawOn(self.canv, self.radius - self.pw / 2, self.radius - self.ph / 2 + self.hoffset)
+        self.canv.circle(self.radius, self.radius,
+                         self.radius, stroke=0, fill=1)
+        self.paragraph.drawOn(
+            self.canv, self.radius - self.pw / 2, self.radius - self.ph / 2 + self.hoffset)
         self.canv.restoreState()
 
 # Draw a horizontal line
