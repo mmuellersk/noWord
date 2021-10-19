88be74344b0076ed78a89a88482fcc80bc1446e0
Matthias Mueller <matthias.mueller@salt.ch>
2021-10-19 09:00:14 +0200

    Fix bug with integer in table (and other value types)

--- noWord/plugins/ForeachBlock/ForeachBlock.py
+++ noWord/plugins/ForeachBlock/ForeachBlock.py
@@ -72,6 +72,8 @@ class ForeachBlock(PluginInterface):
                     if item["id"] not in keysData:
                         continue
 
+            print(name)
+
             index += 1
             context.textCmdProcessors[name] = lambda res: context.getResource(
                 item, res)
